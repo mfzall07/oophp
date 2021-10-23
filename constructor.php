478a584d7bd0c588828f88ef024c46fb49f94ba4
Rias Gauri Nurhasanah <19102212@ittelkom-pwt.ac.id>
2021-10-24 03:42:15 +0700

    oophp

---
+++ constructor.php
@@ -0,0 +1,31 @@
+<?php
+
+class Produk {
+
+    public $judul, 
+           $penulis, 
+           $penerbit, 
+           $harga;
+
+
+    public function __construct($judul, $penulis, $penerbit, $harga){
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+
+    }
+
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
+    
+
+}
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk ("Uncharted", "Neil Druckmen", "Sony Computer", 25000);
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk1->getLabel();
